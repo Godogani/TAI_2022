b8f7a55dc7284e06cc109ddcc07777e0b4d39209
Nochini <93994299+Nochini@users.noreply.github.com>
2022-04-26 08:31:36 -0300

    Add files via upload

--- database/bd.php
+++ database/bd.php
@@ -3,16 +3,23 @@
 class BD
 {
     private $host = "localhost";
+
     private $dbname = "database";
+
     private $port = 3306;
+
     private $user = "root";
+
     private $password = "";
+
     private $db_charset = "utf8";
+
     public function connection()
     {
         $conn = "mysql:host=$this->host;dbname=$this->dbname;port=$this->port";
         return new PDO($conn, $this->user, $this->password, [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->db_charset]);
     }
+
     public function select_data()
     {
         $conn = $this->connection();
@@ -20,6 +27,7 @@ class BD
         $default->execute();
         return $default;
     }
+
     public function inserir($dados)
     {
         $conn = $this->connection();
@@ -30,12 +38,35 @@ class BD
 
         return $st;
     }
-    public function update($dados)
+
+    public function atualizar($dados)
     {
         $conn = $this->connection();
-        $sql = "UPDATE usuario SET 'nome'= ?,'telefone'=?, 'cpf'=? WHERE id=?";
+        $sql = "UPDATE usuario SET 'nome'= ?,'telefone'=?, 'cpf'=? WHERE 'id'=?";
         $st = $conn->prepare($sql);
-        $arrayDados = [$dados['nome'], $dados['cpf'], $dados['telefone']];
+        $arrayDados = [$dados['nome'], $dados['cpf'], $dados['telefone'], $dados['id']];
+        $st->execute($arrayDados);
+
+        return $st;
+    }
+
+    public function deletar($dados)
+    {
+        $conn = $this->connection();
+        $sql = "DELETE FROM usuario WHERE 'id'=?";
+        $st = $conn->prepare($sql);
+        $arrayDados = [$dados['id']];
+        $st->execute($arrayDados);
+
+        return $st;
+    }
+
+    public function buscar($dados)
+    {
+        $conn = $this->connection();
+        $sql = "SELECT * FROM usuario WHERE 'id' = ?";
+        $st = $conn->prepare($sql);
+        $arrayDados = [$dados['id']];
         $st->execute($arrayDados);
 
         return $st;
